bc49a5f133dd3eef5c88d41d54737857260ea61f
kkkzm10 <78135780+kkkzm10@users.noreply.github.com>
2022-10-03 19:26:36 +0900

    create drag&drop function of lists

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -6,7 +6,9 @@
     </header>
     <main>
        <p class="info-line">All: {{ totalCardCount }} tasks</p>
-      <div class="list-index">
+      <draggable :list="lists"
+            @end="movingList"
+            class="list-index">
           <list v-for="(item, index) in lists"
                 :key="item.id"
                 :title="item.title"
@@ -15,19 +17,21 @@
                 @change="movingCard"
           />
           <list-add />
-       </div>
+     </draggable> 
     </main>
   </div>
 
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 import ListAdd from './ListAdd.vue'
 import List from './List'
 import { mapState } from 'vuex'
 
 export default {
   components: {
+    draggable,
     ListAdd,
     List,
   },
@@ -43,6 +47,9 @@ export default {
   movingCard: function() {
     this.$store.dispatch('updateList', { lists: this.lists })
   },
+  movingList: function() {
+    this.$store.dispatch('updateList', { lists: this.lists })
+  }
  }
 }
 </script>
